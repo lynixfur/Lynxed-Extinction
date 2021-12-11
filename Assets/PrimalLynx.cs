3033fdeb2e47878562afe9ae1c3c6f669551fed5
Lynix <lunarlynix@gmail.com>
2021-12-11 08:51:19 -0400

    Fixed Lynx Movement

--- Assets/PrimalLynx.cs
+++ Assets/PrimalLynx.cs
@@ -15,6 +15,12 @@ public class PrimalLynx : MonoBehaviour
     public LayerMask whatIsGround;
     private bool onGround;
 
+    // Primal Movement Controller
+    float horizontalMove = 0f;
+    float walkSpeed = 7f;
+    float jumpHeight = 11f;
+    bool jump = false;
+
     // Start is called before the first frame update
     void Start() // Event BeginPlay (UE4)
     {
@@ -25,7 +31,7 @@ public class PrimalLynx : MonoBehaviour
     void Update() // Event Tick (But instead of CPU tick its frame tick)
     {
 
-        onGround = false;
+        /*onGround = false;
 
         foreach (Transform gc in groundChecks)
             {
@@ -53,12 +59,28 @@ public class PrimalLynx : MonoBehaviour
             if(onGround) {
                 rigidbody2D.AddForce(new Vector2(0,lynxJumpHeight), ForceMode2D.Impulse);
             }
+        }*/
+
+
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+
+            if(Input.GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(-7, 7);
+            }
+            else{
+                transform.localScale = new Vector3(7, 7);
+            }
         }
-    }
 
-    void OnUpdate_lynxmovement_fancystuff() 
-    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime * walkSpeed;
 
+        if(Input.GetButton("Jump") && Mathf.Abs(rigidbody2D.velocity.y) < 0.001f) 
+        {
+            rigidbody2D.AddForce(new Vector2(0,jumpHeight), ForceMode2D.Impulse);
+        }
     }
 
     /* Lynx Networking */
