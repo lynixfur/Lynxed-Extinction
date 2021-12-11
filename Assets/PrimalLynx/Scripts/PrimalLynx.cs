1583cd1f8d7d86d289d96e2f50a1c660141b8fb4
Lynix <lunarlynix@gmail.com>
2021-12-11 13:33:51 -0400

    Added New Movement Controls

--- Assets/PrimalLynx/Scripts/PrimalLynx.cs
+++ Assets/PrimalLynx/Scripts/PrimalLynx.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PrimalLynx : MonoBehaviour
 {
@@ -16,72 +17,69 @@ public class PrimalLynx : MonoBehaviour
     private bool onGround;
 
     // Primal Movement Controller
+    public PlayerInput lynxInput;
+    public LynxControlActions lynxInputActions;
     float horizontalMove = 0f;
     float walkSpeed = 7f;
     float jumpHeight = 11f;
     bool jump = false;
 
-    // Start is called before the first frame update
-    void Start() // Event BeginPlay (UE4)
+
+    private void Awake()
     {
-        
+
+        //lynxInput = new LynxCon
+
+        lynxInputActions = new LynxControlActions();
+        lynxInputActions.Enable();
+        lynxInputActions.Lynx.Jump.performed += Jump;
+        //lynxInputActions.Lynx.Move.performed += Move;
     }
 
-    // Update is called once per frame
-    void Update() // Event Tick (But instead of CPU tick its frame tick)
+    private void PlayerInput_onActionTriggered(InputAction.CallbackContext ctx) {
+        Debug.Log(ctx);
+    }
+
+    void Update()
     {
+        //horizontalMove = Input.GetAxis("Horizontal");
+        Vector2 horizontalMove = lynxInputActions.Lynx.Move.ReadValue<Vector2>();
+        transform.position += new Vector3(horizontalMove.x, 0f, 0f) * Time.deltaTime * walkSpeed;
 
-        /*onGround = false;
-
-        foreach (Transform gc in groundChecks)
-            {
-            if (Physics2D.OverlapCircle(gc.position, groundCheckRadius, whatIsGround))
-            {
-                onGround = true;
-            }
-        }
-
-        if(Input.GetAxis("Horizontal") != 0)
+        if(horizontalMove.x < 0)
         {
-
-            if(Input.GetAxis("Horizontal") < 0)
-            {
-                transform.localScale = new Vector3(-7, 7);
-            }
-            else{
-                transform.localScale = new Vector3(7, 7);
-            }
-
-            rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * lynxSpeed,0));
+            transform.localScale = new Vector3(-7, 7);
         }
-
-        if(Input.GetButton("Jump")) {
-            if(onGround) {
-                rigidbody2D.AddForce(new Vector2(0,lynxJumpHeight), ForceMode2D.Impulse);
-            }
-        }*/
-
-
-        if(Input.GetAxis("Horizontal") != 0)
-        {
-
-            if(Input.GetAxis("Horizontal") < 0)
-            {
-                transform.localScale = new Vector3(-7, 7);
-            }
-            else{
-                transform.localScale = new Vector3(7, 7);
-            }
+        else{
+            transform.localScale = new Vector3(7, 7);
         }
+    }
 
-        horizontalMove = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime * walkSpeed;
+    public void Move(InputAction.CallbackContext ctx) 
+    {
+        Vector2 inputVector = ctx.ReadValue<Vector2>();
+        transform.position += new Vector3(inputVector.x, 0, 0) * Time.deltaTime * walkSpeed;
+    }
 
-        if(Input.GetButton("Jump") && Mathf.Abs(rigidbody2D.velocity.y) < 0.001f) 
+    public void Jump(InputAction.CallbackContext ctx) 
+    {
+        if(Mathf.Abs(rigidbody2D.velocity.y) < 0.001f) 
         {
             rigidbody2D.AddForce(new Vector2(0,jumpHeight), ForceMode2D.Impulse);
+            Debug.Log("PrimalLynx: Client Controller Jumped!");
         }
     }
 
     /* Lynx Networking */
+    /* Dedicated Servers are not officially supported by Lynxed Extinction right now! */
+
+    public string GetUsername() {
+        //return pgd.NetworkUsername();
+        return "Username";
+    }
+
+    public string GetUserID() {
+        //return pgd.NetworkIdentifcation();
+        return "0";
+    }
 }
